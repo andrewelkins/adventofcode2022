c446f1af9cf1290e31c40c5cb28a053432efe1c9
Andrew Elkins <aelkins@auditboard.com>
2022-12-05 23:53:15 -0800

    Add colors

--- 06/puzzle.py
+++ 06/puzzle.py
@@ -1,4 +1,4 @@
-import os, sys 
+import os, sys, time
 sys.path.append("..")
 try:
     from helpers import helpers
@@ -8,14 +8,22 @@ except ModuleNotFoundError:
 
 DAY = os.path.basename(os.getcwd())
 
+colors = ['\033[95m', '\033[94m', '\033[92m', '\033[93m', '\033[92m', '\033[91m', '\033[90m', '\033[0m', '\033[1m', '\033[4m']
+
+def clear_lines():
+    _ = os.system('clear')   # delete the last line
+
 def parse_scan(puzzle, scan_length):
     input_length = len(puzzle)
     scan_length_index = scan_length - 1
     for i in range(scan_length_index, input_length):
         scan = puzzle[i-scan_length_index:i+1]
+        clear_lines()
+        print(colors[5]+puzzle[:i-scan_length_index]+'\033[0m  '+colors[2]+scan+'  \033[0m'+puzzle[i+1:]+'\033[0m')
         scan_set = set(scan)
         if len(scan_set) == scan_length:
             return i+1
+        time.sleep(0.01)
 
 def part1():
     print("Part 1")
@@ -32,6 +40,7 @@ if __name__ == "__main__":
     print("Day " + DAY)
     print("=====")
     part1()
+    time.sleep(1)
     print("=====")
     part2()
     print("=====")
